b33bb8454c7fe8a8675782fc64cdb5c8a70539d1
Tyler Randall <tyler7r@gmail.com>
2023-08-24 13:31:32 -0400

    Add images and start building out homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Header } from './Header';
 import { Home } from './Home';
@@ -6,12 +6,35 @@ import { Contact } from './Contact';
 import { Projects } from './Projects';
 
 function App() {
+  const [mobileView, setMobileView] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth >= 480) {
+      setMobileView(false)
+    } else {
+      setMobileView(true);
+    }
+  }
+
+  useEffect(() => {
+    if (window.innerWidth <= 480) {
+      setMobileView(true)
+    }
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return _ =>{
+      window.removeEventListener('resize', handleResize);
+    }
+  })
+
   return (
       <BrowserRouter>
         <div className="App">
-          <Header />
+          <Header mobileView={mobileView}/>
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<Home mobileView={mobileView} />} />
             <Route path='/projects' element={<Projects />} />
             <Route path='/contact' element={<Contact />} />
           </Routes>
--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import MenuIcon from './images/three.svg'
 
 export const Header = (props) => {
-    const size = window.innerWidth
-    useEffect(() => {
-        console.log(size);
-    }, [size])
+    const { mobileView } = props
+    const [menuOpen, setMenuOpen] = useState(false);
+    
+    const menuToggle = () => {
+        setMenuOpen(!menuOpen);
+    }
 
     return (
-        <header>
-            <div id='header-name'></div>
-        </header>
+        <>
+            {mobileView === true &&
+                <header id='topnav'>
+                    <div id='header-name'>Tyler Randall</div>
+                    <img src={MenuIcon} onClick={() => menuToggle()}/>
+                    {menuOpen === true &&
+                        <div className="overlay">
+                            <div id='my-links'>
+                                <Link className='mobile-link' to='/'>Home</Link>
+                                <Link className='mobile-link' to='/projects'>Projects</Link>
+                                <Link className='mobile-link' to='/contact'>Contact</Link>
+                            </div>
+                        </div>
+                    }
+                </header>
+            }
+            {mobileView === false && 
+                <header id='topnav'>
+                    <div id='header-name'>Tyler Randall</div>
+                        <div id='my-links'>
+                            <Link className='mobile-link' to='/'>Home</Link>
+                            <Link className='mobile-link' to='/projects'>Projects</Link>
+                            <Link className='mobile-link' to='/contact'>Contact</Link>
+                        </div>
+                </header>
+            }
+        </>
     )
 }
