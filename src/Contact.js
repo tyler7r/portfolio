6decf5d20cf7a42aee6f1d2dd0fc861b439d9a4f
Tyler Randall <tyler7r@gmail.com>
2023-08-26 01:25:07 -0400

    Minor styling fixes

--- src/Contact.js
+++ src/Contact.js
@@ -6,17 +6,19 @@ export const Contact = (props) => {
         <div id='contact-section'>
             <div id='contact-title'>Contact</div>
             <div id='contact-message'>Looking forward to hearing from you.</div>
-            <div className='contact-info-container' id='phone-container'>
-                <div className='contact-info-title'>
-                    Phone
+            <div id="contact-links-container">
+                <div className='contact-info-container' id='phone-container'>
+                    <div className='contact-info-title'>
+                        Phone
+                    </div>
+                    <a href='tel:4049847244' id='phone'>404-984-7244</a>
                 </div>
-                <a href='tel:4049847244' id='phone'>404-984-7244</a>
-            </div>
-            <div className='contact-info-container' id='email-container'>
-                <div className='contact-info-title'>
-                    Email
+                <div className='contact-info-container' id='email-container'>
+                    <div className='contact-info-title'>
+                        Email
+                    </div>
+                    <a href='mailto:tyler7r@gmail.com' id='email'>tyler7r@gmail.com</a>
                 </div>
-                <a href='mailto:tyler7r@gmail.com' id='email'>tyler7r@gmail.com</a>
             </div>
         </div>
     )
--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ export const Home = (props) => {
         const possibleGreetings = [
             'Hello', 'Hola', 'Bonjour', 'Ciao', 'Olá', "G'day", 'Geia', 'Zdravo', 'Privet', 'Nǐ hǎo', "Kon’nichiwa", 'Guten tag'
         ]
-        
+
         const randomGreeting = () => {
             const randomNum = Math.floor(Math.random() * 12);
             setGreeting(possibleGreetings[randomNum])
@@ -28,15 +28,17 @@ export const Home = (props) => {
         <div id='homepage'>
             <div id="homepage-content">
                 <img id='headshot' alt='headshot' src={Headshot} />
-                <div id='about-me-container'>
-                    <div id='about-me-hello'>{greeting}</div>
-                    <div id='about-me-description'>About Me</div>
-                    <p id='about-me'>I am a developer from Atlanta, Georgia who is passionate about building sites with simple solutions and in-depth, user-friendly UI. I recently completed The Odin Project and I wanted to display some of the work that I completed for it. The majority of these projects are completed with a combination of HTML, CSS, Javascript, React, Node, Express and MongoDB. My work demonstrates my proficiency with these skills and highlights some of my passions outside of development as well. Thanks for checking it out!</p>
-                </div>
-                <div id='home-links-container'>
-                    <Link id='projects-link' className='home-link' to='/projects'>Projects</Link>
-                    <Link id='resume-link' className='home-link' to='/resume'>Resume</Link>
-                    <Link id='contact-link' className='home-link' to='/contact'>Contact</Link>
+                <div id="about-me-info">
+                    <div id='about-me-container'>
+                        <div id='about-me-hello'>{greeting}</div>
+                        <div id='about-me-description'>About Me</div>
+                        <p id='about-me'>I am a developer from Atlanta, Georgia who is passionate about building sites with simple solutions and in-depth, user-friendly UI. I recently completed The Odin Project and I wanted to display some of the work that I completed for it. The majority of these projects are completed with a combination of HTML, CSS, Javascript, React, Node, Express and MongoDB. My work demonstrates my proficiency with these skills and highlights some of my passions outside of development as well. Thanks for checking it out!</p>
+                    </div>
+                    <div id='home-links-container'>
+                        <Link id='projects-link' className='home-link' to='/projects'>Projects</Link>
+                        <Link id='resume-link' className='home-link' to='/resume'>Resume</Link>
+                        <Link id='contact-link' className='home-link' to='/contact'>Contact</Link>
+                    </div>
                 </div>
             </div>
         </div>
